98d0e1639a4e0731e1886dc1f7beee1e6fb27371
Alexandr Shulgin <chenneldps@gmail.com>
2022-12-03 20:07:59 +0300

    deployment

--- server/db.js
+++ server/db.js
@@ -1,12 +1,23 @@
 const {Sequelize} = require('sequelize');
 
-module.exports = new Sequelize(
-    process.env.DB_NAME,
+module.exports = new Sequelize('postgres://shhh:qAmfYi8FfDYcCdrqihfUAsLTw9T27gz8@dpg-ce5nbtda4991uet16l90-a.frankfurt-postgres.render.com/bm',
+
+    {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: 'false'
+            }
+        },
+    }
+
+    /*process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
         dialect: 'postgres',
         host: process.env.DB_HOSTNAME,
         port: process.env.DB_PORT,
-    }
+    }*/
 )
